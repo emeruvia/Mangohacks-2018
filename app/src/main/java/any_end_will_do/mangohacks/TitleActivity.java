e0347d3a891f42a6c7549b1e37fdecfbc5bd2771
jeffrey <jeffreyfleurent@outlook.com>
2018-02-03 09:14:36 -0500

    implemented bottom navigation

--- app/src/main/java/any_end_will_do/mangohacks/TitleActivity.java
+++ app/src/main/java/any_end_will_do/mangohacks/TitleActivity.java
@@ -1,7 +1,9 @@
 package any_end_will_do.mangohacks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void testingClick(View view) {
+        Intent intent = new Intent(this, MainPageActivity.class);
+        startActivity(intent);
+    }
+
 }
